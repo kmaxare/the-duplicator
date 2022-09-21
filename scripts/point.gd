0a5965c89d79461d4bc61241d4903e4f31f47ba1
Kevin Arenas <kevinarenas@unach.cl>
2022-09-21 12:18:39 -0400

    Nivel Dos terminado - Adicion de fondo en nivel uno - Arreglos en nodo POINT para el respaw de puntos

--- scripts/point.gd
+++ scripts/point.gd
@@ -5,7 +5,6 @@ export var time_respawn = 4
 export var tope_spawn = 10
 
 export var point = 1
-var boot_activo = true
 
 export (Texture) var folder
 export (Texture) var archivo
@@ -17,18 +16,17 @@ func _ready():
 	$anim_npc.play("dance")
 
 func _on_point_body_entered(body):
-	if body.is_in_group("personaje") and boot_activo:
+	if body.is_in_group("personaje") and visible:
 		body.cont_copias('addition', point) # Sumamos mas uno al personaje
 		$SFX/comer.play()
 		disable()
 		
 func disable():
 	visible = false
-	boot_activo = false
 	#anim desaparecer
 	yield(get_tree().create_timer(time_respawn), "timeout")
 	if tipe_point == 1 or tope_spawn == 1: queue_free()
 	#anim activar
-	tope_spawn -= 1
-	boot_activo = true
-	visible = true
+	else:
+		tope_spawn -= 1
+		visible = true
